44899911e390ea6c873dbba581d52c253bcc85c5
jlferrete <joseluisferrete@gmail.com>
2023-09-04 20:52:19 +0200

    Passing tests

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,16 +1,17 @@
 // importar modelo
+import { TaskModel } from '../models/task.js'
 
 export const getAllTasks = (req, res) => {
   const tasks = TaskModel.getAllTasks()
   res.json(tasks)
 }
 
-export const addTask = (req, res) => {
-  const task = // ????
+export const addTask = async ({ body }, res) => {
+  const task = await TaskModel.addTask(body.description)
   res.json(task)
 }
 
-export const deleteTask = (req, res) => {
-  // ????
+export const deleteTask = ({ params }, res) => {
+  TaskModel.deleteTask(params.id)
   res.json({ message: 'Deleted successfully' })
 }
--- models/task.js
+++ models/task.js
@@ -2,7 +2,14 @@
  * Array to store tasks.
  * @type {Array<Object>}
  */
-const tasks = []
+
+/** 
+ * He tenido que investigar como lo resolvieron otros compañeros
+ * Dejando el const, fallaban los tests y supongo que es
+ * por el ambito de las funciones. Pero me genera dudas.
+*/ 
+
+let tasks = []
 
 /**
  * ID for the next task. Increments each time a new task is added.
@@ -14,10 +21,7 @@ let currentId = 1
  * Model class representing a task.
  * @class
  */
-
- export class TaskModel {
- /*
- *
+export class TaskModel {
   /**
    * Fetches all tasks.
    * @name TaskModel.getAllTasks
@@ -25,21 +29,48 @@ let currentId = 1
    * @returns {Array<Object>} Array of tasks.
    */
 
+  /** 
+   * Otros compañeros han usado async para esto
+   * Entiendo que no es necesario en este caso, al igual que el delete
+   * Pero te agredecería que me corrigieras si estoy equivocado 
+   */ 
   static getAllTasks () {
-   return tasks
- }
+    return tasks
+  }
 
-/**
+  /**
    * Adds a new task to the list.
    * @name TaskModel.addTask
    * @static
    * @param {string} description - The description of the task.
    * @returns {Object} The new task object.
    */
+  static async addTask (description = '') {
+    if (!description) {
+      return {}
+    }
 
-/**
+    const task = {
+      id: currentId,
+      description
+    }
+
+    currentId++
+
+    tasks.push(task)
+
+    return task
+  }
+
+  /**
    * Deletes a task from the list by its ID.
    * @name TaskModel.deleteTask
    * @static
    * @param {number} id - The ID of the task to delete.
    */
+  static async deleteTask (id = 0) {
+    const removeTask = tasks.filter((task) => task.id !== Number(id))
+    tasks = removeTask
+    return tasks
+  }
+}
